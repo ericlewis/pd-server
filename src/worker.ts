49fd6e604b0bfeca67fd907c12c2c065f4b404dc
Eric Lewis <ericlewis777@gmail.com>
2023-09-01 10:56:57 -0400

    fix: urls redirect properly

--- src/worker.ts
+++ src/worker.ts
@@ -2,12 +2,13 @@ export default {
 	async fetch(request: Request) {
 		const url = new URL(request.url);
 		const { pathname, searchParams } = url;
+
 		if (pathname === '/api/v2/firmware/') {
 			const response = await fetch('https://github.com/scratchminer/pd-ota/releases/latest/download/update.json');
 			const update = JSON.parse(new TextDecoder('utf-8').decode(await response.arrayBuffer()));
 
-			if (searchParams.get("current_version") === update.version) {
-			return new Response(null, { status: 204 });
+			if (searchParams.get('current_version') === update.version) {
+				return new Response(null, { status: 204 });
 			}
 
 			const url = (await fetch(update.url, { redirect: 'manual' })).headers.get('location');
@@ -15,19 +16,24 @@ export default {
 			return new Response(JSON.stringify({ ...update, url }), {
 				headers: { 'content-type': 'application/json' },
 			});
-		} else {
-			// redirect everything else to panic
-			const newUrl = new URL(url);
-			newUrl.hostname = 'play.date';
-			const newReq = new Request(newUrl, request);
-			newReq.headers.set('host', 'play.date');
-			newReq.headers.delete('cf-connecting-ip');
-			newReq.headers.delete('cf-ipcountry');
-			newReq.headers.delete('cf-ray');
-			newReq.headers.delete('cf-visitor');
-			newReq.headers.delete('x-forwarded-proto');
-			newReq.headers.delete('x-real-ip');
-			return fetch(newReq);
 		}
+
+		// redirect everything else to panic
+		const newUrl = new URL(url);
+		newUrl.hostname = 'play.date';
+
+		const newReq = new Request(newUrl, request);
+		newReq.headers.set('host', 'play.date');
+		newReq.headers.delete('cf-connecting-ip');
+		newReq.headers.delete('cf-ipcountry');
+		newReq.headers.delete('cf-ray');
+		newReq.headers.delete('cf-visitor');
+		newReq.headers.delete('x-forwarded-proto');
+		newReq.headers.delete('x-real-ip');
+
+		const response = await fetch(newReq);
+		const responseText = await response.text();
+		const fixedResponseText = responseText.replaceAll(`"https://play.date/api/`, `"https://sydh.date/api/`);
+		return new Response(fixedResponseText, response);
 	},
 };
